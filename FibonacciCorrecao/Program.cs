e3b8548e5e55758347978579a5e5d3fd681b52d2
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-03 14:05:27 -0300

    Add project files.

---
+++ FibonacciCorrecao/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double ant = 1, prox = 0;
+        //double atual = 1;
+        int posicao;
+
+        Console.WriteLine("Informe a posicao desejada: ");
+        posicao = int.Parse(Console.ReadLine());
+
+        /*
+        for(int i =0; i<=posicao;i++)
+        {
+            atual = ant + prox;
+            ant = prox;
+            prox = atual;
+            Console.Write(atual + " ");
+        }
+        */
+
+        for(int i = 0; i < posicao; i++)
+        {
+            Console.Write(ant + prox + " ");
+            prox = ant + prox;
+            ant = prox - ant;
+        }
+       
+
+    }
+}
